16f871598de2d8f59e9a525749d82ec74d9026f7
Quevin Bambasi <108383036+quevinrai@users.noreply.github.com>
2023-07-04 15:29:52 -0400

    Add print lines and create class 'Solution' instance

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
         pass
+
+s = "MCMXIV"
+sol = Solution()
+
+print("ROMAN NUMERAL TO INTEGER")
+print(f"Roman Numeral: {s}")
+print(f"Integer: {sol.romanToInt(s)}")
